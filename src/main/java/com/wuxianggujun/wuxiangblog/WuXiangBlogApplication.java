e3bd0ef39e0f4efee00c960b505070baf8a15e7d
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-11 02:50:54 +0800

    创建一个springboot项目，使用了阿里云的druid库和一些其他的技术，将springboot的版本布置成了2.7版本。

---
+++ src/main/java/com/wuxianggujun/wuxiangblog/WuXiangBlogApplication.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.wuxiangblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuXiangBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuXiangBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuxianggujun/wuxiangblog/WuXiangBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.wuxiangblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuXiangBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
