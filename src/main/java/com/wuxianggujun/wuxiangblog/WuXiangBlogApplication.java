bb420cda71549a10cdef488e649266d82564e26a
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-16 14:30:04 +0800

    配置mybatis的xml与Java类的绑定

--- src/main/java/com/wuxianggujun/wuxiangblog/WuXiangBlogApplication.java
+++ src/main/java/com/wuxianggujun/wuxiangblog/WuXiangBlogApplication.java
@@ -1,9 +1,11 @@
 package com.wuxianggujun.wuxiangblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wuxianggujun.wuxiangblog.mapper")
 public class WuXiangBlogApplication {
 
     public static void main(String[] args) {
