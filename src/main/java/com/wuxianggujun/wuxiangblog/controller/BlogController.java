a3413f426c300f28b6dde0e3e4df56f3d1819e8f
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-22 10:52:26 +0800

    用yaml重写配置文件

--- src/main/java/com/wuxianggujun/wuxiangblog/controller/BlogController.java
+++ src/main/java/com/wuxianggujun/wuxiangblog/controller/BlogController.java
@@ -1,10 +1,15 @@
 package com.wuxianggujun.wuxiangblog.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wuxianggujun.wuxiangblog.entity.Blog;
 import com.wuxianggujun.wuxiangblog.result.Result;
+import com.wuxianggujun.wuxiangblog.result.ResultGenerator;
 import com.wuxianggujun.wuxiangblog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/blog")
@@ -23,8 +28,12 @@ public class BlogController {
 
 
     @GetMapping("/search")
-    public Result search() {
-        return null;
+    public Result search(@RequestParam(defaultValue = "1", value = "pageNum") Integer pageNum) {
+        //分页插件,返回分页数据
+        List<Blog> blogs = blogService.searchBlogs();
+        PageHelper.startPage(pageNum, 10);
+        PageInfo<Blog> pageInfo = new PageInfo<>(blogs);
+        return ResultGenerator.getSuccessResult(pageInfo);
     }
 
     @PostMapping("/{id}/update")
